eb9bb47f98b4fb47ec666ac2a4aebdc459a09304
Egmon Pereira <egmonpereira@gmail.com>
2021-06-03 19:51:44 -0300

    Sem comentários extras

--- entrada.py
+++ entrada.py
@@ -17,10 +17,6 @@ class Entrada():
         for i in range(n):
             t = float(input())
             Termos.append(t)
-        #print("Entre com os Coeficientes do Sistema Linear:")
-#        dados = [[4,3,3,1],[2,-3,5,-1],[1,-1,1,-1],[-2,-1,1,4]]#input().split(' ')
-        #print("Entre com os Termos Independentes do Sistema Linear:")
-        #Termos = [3,2,0,-2]#input().split(' ')
 
         for i in dados:
             Matriz.append(i)
