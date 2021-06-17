3ee812b09c507c26770bbd1daaaca94b50e51a48
Egmonpereira <egmonpereira@gmail.com>
2021-06-17 17:10:49 -0300

    Versão final para entregar ao professor

--- calculo.py
+++ calculo.py
@@ -1,15 +1,21 @@
+from os import linesep
 import numpy
+import pandas as pd
 
 class Calculo():
     def funcao(Matriz,Termos):
         Deter = []
         aux = []
+        df = pd.DataFrame(Matriz)
 
         Deter.append(numpy.linalg.det(Matriz))
         if Deter[0] != 0:
             #Calcula o Determinante das Colunas x1, ... xn
-            print("Coeficientes =",Matriz)
-            print("Termos independentes =", Termos)
+            print("\nCoeficientes:")
+            print(pd.DataFrame(Matriz, columns=[" "," "], index=["X1 =","X2 ="]))
+            print("\nTermos independentes:")
+            print(pd.DataFrame(Termos, columns=[" "], index=["T1 =","T2 ="]))
+
             t = 0
             for i in range(len(Matriz)):
                 for j in range(len(Matriz[0])):
@@ -21,8 +27,12 @@ class Calculo():
                 Deter.append(numpy.linalg.det(aux))
                 t = t + 1
                 aux.clear()
+            for i in range(8):
+                print("=",end="")
+                print("-",end="")
+            print("=",end="")
 
-            print("Valores Reais:")
+            print("\nValores Reais:")
             for i in range(len(Matriz)):
                 print("X",i+1,"=", round(Deter[i+1]/Deter[0],2))
         else:
